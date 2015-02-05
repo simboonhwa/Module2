20595c7ae6c88f20fd4236189608fd03ef74775e
Sim Boon Hwa <simboonhwa@rocketmail.com>
2015-02-05 11:02:31 +0800

    9/10 tested successful

--- complete.R
+++ complete.R
@@ -2,15 +2,17 @@ complete <- function(directory, id = 1:332) {
 
 	source("consolidateDF.R")
 	MYTABLE <- consolidateDF(directory,id)
-
+	# return row with df wo NA
+	MYTABLE <- MYTABLE[complete.cases(MYTABLE),]
+	
 	#return DF based on 
 	RESULT <- aggregate(MYTABLE$ID, list(id=MYTABLE$ID), length)
 
 	# re-order the output based on the input (id)
-	ORESULT <- RESULT[match(RESULT$id,id),]
+	ORESULT <- RESULT[match(id,RESULT$id),]
 
 	#rename the row & column accordingly
-	colnames(RESULT) <- c("id","nobs")
+	colnames(ORESULT) <- c("id","nobs")
 	rownames(ORESULT) <- c(1:nrow(ORESULT))
 	ORESULT
 }
--- corr.R
+++ corr.R
@@ -5,11 +5,15 @@ corr <- function(directory, threshold = 0) {
 	LIST <- list()
 	# return DF with station & nobs >= threshold
 	STATION <- subset(complete(directory), nobs  >= threshold, select=id)
+	
 	if (nrow(STATION) != 0){
 		# no station selected will return emply list
 
 		# return DF with only the given STATION id
 		MYTABLE <- consolidateDF(directory,STATION$id)
+
+		# return row with df wo NA
+		MYTABLE <- MYTABLE[complete.cases(MYTABLE),]
 
 		# group the station and compute the cor accordingly and return the list
 		LIST <- lapply(split(MYTABLE, MYTABLE$ID), function(x) cor(x$sulfate, x$nitrate))
